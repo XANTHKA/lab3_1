d03c6bee8ababbaedb37e3a6849db325214c0586
xanthka <agnieszka.tarnowska@forweb.pl>
2016-02-08 23:18:40 +0100

    deklaracja zmiennych

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -2,10 +2,15 @@ package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
 import static org.junit.Assert.*;
 
+import java.util.Date;
+
 import org.junit.Test;
 
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class BookKeeperTest {
 
@@ -13,11 +18,24 @@ public class BookKeeperTest {
 
 	@Test
 	public void test() {
-		InvoiceFactory invoiceFactory = new InvoiceFactory();
-		bk = new BookKeeper(invoiceFactory);
-		Invoice invoice;
-		InvoiceRequest invoiceRequest = new InvoiceRequest(new ClientData(new Id("123"),"aaaa"));
-		invoiceRequest.addItem(new InvoiceLine(new ProductData(), 0, null, null));
+	InvoiceFactory invoiceFactory = new InvoiceFactory();
+	bk = new BookKeeper(invoiceFactory);
+	Invoice invoice;
+		Id numerKlienta = new Id("123");
+		String nazwaKlienta = "pierwszy klient";
+		ClientData klientA = new ClientData(numerKlienta, nazwaKlienta );
+	InvoiceRequest invoiceRequest = new InvoiceRequest(klientA);
+		Money cenaProduktu = new Money(1);
+		String nazwaProduktu = "pierwszy produkt";
+		ProductType typProduktu = ProductType.FOOD;
+		Id numerProduktu = new Id("1");
+		Date data = new Date();
+	ProductData pierwszyprodukt = new ProductData(numerProduktu, cenaProduktu, nazwaProduktu, typProduktu, data);
+		int ilosc = 3;
+		Money wartoscNetto;
+		Tax podatek;		
+	InvoiceLine nowaLinia = new InvoiceLine(pierwszyprodukt, ilosc, wartoscNetto, podatek);
+		invoiceRequest.addItem(nowaLinia);
 		TaxPolicy taxPolicy = null;
 		//Invoice invoice = invoiceFactory.create(invoiceRequest.getClientData());
 		invoice = bk.issuance(invoiceRequest, taxPolicy);

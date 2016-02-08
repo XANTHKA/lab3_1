86210edd9f43acb41c3f0b4da1ba82dc722a9eb7
xanthka <agnieszka.tarnowska@forweb.pl>
2016-02-08 22:27:31 +0100

    deklaracja obiektów

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -4,16 +4,22 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+
 public class BookKeeperTest {
 
 	private BookKeeper bk;
 
 	@Test
 	public void test() {
-		InvoiceFactory invoiceFactory = null;
+		InvoiceFactory invoiceFactory = new InvoiceFactory();
 		bk = new BookKeeper(invoiceFactory);
-		
-		
+		Invoice invoice;
+		InvoiceRequest invoiceRequest = new InvoiceRequest(new ClientData(new Id("123"),"aaaa"));
+		invoiceRequest.addItem(new InvoiceLine(new ProductData(), 0, null, null));
+		TaxPolicy taxPolicy = null;
+		//Invoice invoice = invoiceFactory.create(invoiceRequest.getClientData());
+		invoice = bk.issuance(invoiceRequest, taxPolicy);
+		System.out.print(invoice.getGros());
 	}
-
-}

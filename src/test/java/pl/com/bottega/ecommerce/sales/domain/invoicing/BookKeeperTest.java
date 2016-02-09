282372f0f454f45590dbbb2af77cc5bf3fe49df7
xanthka <agnieszka.tarnowska@forweb.pl>
2016-02-09 21:57:37 +0100

    mockowanie class

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -5,9 +5,11 @@ import static org.junit.Assert.*;
 import java.util.Date;
 
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.Product;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
@@ -18,26 +20,47 @@ public class BookKeeperTest {
 
 	@Test
 	public void test() {
-	InvoiceFactory invoiceFactory = new InvoiceFactory();
-	bk = new BookKeeper(invoiceFactory);
-	Invoice invoice;
-		Id numerKlienta = new Id("123");
-		String nazwaKlienta = "pierwszy klient";
-		ClientData klientA = new ClientData(numerKlienta, nazwaKlienta );
-	InvoiceRequest invoiceRequest = new InvoiceRequest(klientA);
-		Money cenaProduktu = new Money(1);
-		String nazwaProduktu = "pierwszy produkt";
-		ProductType typProduktu = ProductType.FOOD;
-		Id numerProduktu = new Id("1");
-		Date data = new Date();
-	ProductData pierwszyprodukt = new ProductData(numerProduktu, cenaProduktu, nazwaProduktu, typProduktu, data);
-		int ilosc = 3;
-		Money wartoscNetto;
-		Tax podatek;		
-	InvoiceLine nowaLinia = new InvoiceLine(pierwszyprodukt, ilosc, wartoscNetto, podatek);
-		invoiceRequest.addItem(nowaLinia);
-		TaxPolicy taxPolicy = null;
-		//Invoice invoice = invoiceFactory.create(invoiceRequest.getClientData());
-		invoice = bk.issuance(invoiceRequest, taxPolicy);
-		System.out.print(invoice.getGros());
+		
+		InvoiceFactory invoiceFactory = new InvoiceFactory();
+		bk = new BookKeeper(invoiceFactory);
+		Invoice invoice;
+		InvoiceRequest mockInvoiceRequest = Mockito.mock(InvoiceRequest.class); 
+		TaxPolicy mockTaxPolicy = Mockito.mock(TaxPolicy.class);
+		InvoiceLine mockInvoiceLine = Mockito.mock(InvoiceLine.class);
+		InvoiceRequest mockInvoiceRequest = mockito.mock
+		
+		
+				
+				
+				
+		invoice = bk.issuance(mockInvoiceRequest, mockTaxPolicy);
+		System.out.print(invoice.getGros());	
+		
+		
+		
+//	InvoiceFactory invoiceFactory = new InvoiceFactory();
+//	bk = new BookKeeper(invoiceFactory);
+//	Invoice invoice;
+//		Id numerKlienta = new Id("123");
+//		String nazwaKlienta = "pierwszy klient";
+//		ClientData klientA = new ClientData(numerKlienta, nazwaKlienta );
+//	InvoiceRequest invoiceRequest = new InvoiceRequest(klientA);
+//		Money cenaProduktu = new Money(1);
+//		String nazwaProduktu = "pierwszy produkt";
+//		ProductType typProduktu = ProductType.FOOD;
+//		Id numerProduktu = new Id("1");
+//		Date data = new Date();
+//	ProductData pierwszyprodukt = new ProductData(numerProduktu, cenaProduktu, nazwaProduktu, typProduktu, data);
+//		int ilosc = 3;
+//		Money wartoscNetto;
+//		Tax podatek;	
+//	Product mpierwszyProdukt = new Product();
+//	
+//	InvoiceLine nowaLinia = new InvoiceLine(pierwszyprodukt, ilosc, wartoscNetto, podatek);
+//		invoiceRequest.addItem(nowaLinia);
+//		TaxPolicy taxPolicy = null;
+//		//Invoice invoice = invoiceFactory.create(invoiceRequest.getClientData());
+//		invoice = bk.issuance(invoiceRequest, taxPolicy);
+//		System.out.print(invoice.getGros());
 	}
+}

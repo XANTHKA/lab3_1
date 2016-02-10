6f966415e26164859d8dd53a912f010771b51edd
xanthka <agnieszka.tarnowska@forweb.pl>
2016-02-10 01:38:10 +0100

    zakończone mockowanie

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -3,15 +3,18 @@ package pl.com.bottega.ecommerce.sales.domain.invoicing;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.when;
 
+import java.awt.List;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
 
+
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Matchers;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
-
 
 
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
@@ -33,49 +36,18 @@ public class BookKeeperTest {
 		Invoice invoice;
 		TaxPolicy mockTaxPolicy = Mockito.mock(TaxPolicy.class);
 		InvoiceRequest mockInvoiceRequest = Mockito.mock(InvoiceRequest.class);
-		ProductData mocProductData = Mockito.mock(ProductData.class);
-		//when(mockProductData.getProductData()).thenReturn(1, 1.0, "pierwszyprodukt", "typ", 01-01-2015);
-		Collection<RequestItem> kolekcja = new <RequestItem>();
-		(mocProductData, 2, 2.0);
+		ProductData mockProductData = Mockito.mock(ProductData.class);
+		RequestItem mockRequestItem = Mockito.mock(RequestItem.class);
+		when(mockTaxPolicy.calculateTax(Matchers.any(ProductType.class), Matchers.any(Money.class))).thenReturn(new Tax(new Money(0.0), "VAT"));
+		when(mockProductData.getType()).thenReturn(ProductType.DRUG);
+		when(mockRequestItem.getTotalCost()).thenReturn(new Money (1.0));
+		when(mockRequestItem.getProductData()).thenReturn(mockProductData);
+		ArrayList<RequestItem> kolekcja = new ArrayList<RequestItem>();
+		kolekcja.add(mockRequestItem);
 		when(mockInvoiceRequest.getItems()).thenReturn(kolekcja);
-		
-		
-		
-		
-		
-				
-				
-				
 		invoice = bk.issuance(mockInvoiceRequest, mockTaxPolicy);
 		//Assert
 		System.out.print(invoice.getGros());	
 		
-		
-		
-		
-//	InvoiceFactory invoiceFactory = new InvoiceFactory();
-//	bk = new BookKeeper(invoiceFactory);
-//	Invoice invoice;
-//		Id numerKlienta = new Id("123");
-//		String nazwaKlienta = "pierwszy klient";
-//		ClientData klientA = new ClientData(numerKlienta, nazwaKlienta );
-//	InvoiceRequest invoiceRequest = new InvoiceRequest(klientA);
-//		Money cenaProduktu = new Money(1);
-//		String nazwaProduktu = "pierwszy produkt";
-//		ProductType typProduktu = ProductType.FOOD;
-//		Id numerProduktu = new Id("1");
-//		Date data = new Date();
-//	ProductData pierwszyprodukt = new ProductData(numerProduktu, cenaProduktu, nazwaProduktu, typProduktu, data);
-//		int ilosc = 3;
-//		Money wartoscNetto;
-//		Tax podatek;	
-//	Product mpierwszyProdukt = new Product();
-//	
-//	InvoiceLine nowaLinia = new InvoiceLine(pierwszyprodukt, ilosc, wartoscNetto, podatek);
-//		invoiceRequest.addItem(nowaLinia);
-//		TaxPolicy taxPolicy = null;
-//		//Invoice invoice = invoiceFactory.create(invoiceRequest.getClientData());
-//		invoice = bk.issuance(invoiceRequest, taxPolicy);
-//		System.out.print(invoice.getGros());
 	}
 }
